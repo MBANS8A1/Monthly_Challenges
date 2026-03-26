65093e752457394395f2c8eef249d8f51a9e545b
MBANS8A1 <seanmbng@aol.com>
2026-03-26 23:25:17 +0000

    In challenges/urls.py instead of specifying individual path entries in the URLConfih (urlpatterns list), I specified a path with a the month dynamic segment, which will help route the request with different concrete month values to the monthly_challenge view function in challenges/views.py. The monthly_challenge view function uses if-elif-else blocks to initialize the challenge_text variable depending on the month parameter entered. If the month is valid a HttpResponse is generated, otherwise a Http 404 error is generated (HttpResponseNotFound).

--- challenges/views.py
+++ challenges/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 # Create your views here.
 
 
-def january(request):
-    return HttpResponse("Walk for at least 20 minutes a day!")
-
-
-def february(request):
-    return HttpResponse("Learn 200 words of a new language!")
-
-
-def march(request):
-    return HttpResponse("Follow a vegetarian diet for the whole month!")
+def monthly_challenge(request, month):
+    challenge_text = None
+    if month == "january":
+        challenge_text = "Walk for at least 20 minutes a day!"
+    elif month == "february":
+        challenge_text = "Learn 200 words of a new language!"
+    elif month == "march":
+        challenge_text = "Follow a vegetarian diet for the whole month!"
+    else:
+        challenge_text = "This month is not supported!"
+        HttpResponseNotFound(challenge_text)
+    return HttpResponse(challenge_text)
