5f30b780be66950cb478b5e27fd8ba9c8183c9a1
MBANS8A1 <seanmbng@aol.com>
2026-03-27 11:55:19 +0000

    I removed the if-elif-else block from the monthly_challenge view function and used a try/except block where the challenge text is set using a monthly_challenges dictionary, depending on the value of the month parameter, which acts as a key to the dictionary.

--- challenges/views.py
+++ challenges/views.py
@@ -2,20 +2,29 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound
 # Create your views here.
 
+monthly_challenges = {
+    "january": "Walk for at least 20 minutes a day!",
+    "february": "Learn 200 words of a new language!",
+    "march": "Follow a vegetarian diet for the whole month!",
+    "april": "Learn Django for 20 minutes a day!",
+    "may": "Take up a new hobby!",
+    "june": "Take up jogging every two days in the mornings!",
+    "july": "Take a trip to another part of the country for the weekend!",
+    "august": "Learn how to drive a motorcycle!",
+    "september": "Create a discord channel for Python programming!",
+    "october": "Create a social media account for you programming work!",
+    "november": "Read a new novel from your favourite genre!",
+    "december": "Cook something else for Christmas other than turkey!"
+}
+
 
 def monthly_challenge_by_number(request, month):
     return HttpResponse(month)
 
 
 def monthly_challenge(request, month):
-    challenge_text = None
-    if month == "january":
-        challenge_text = "Walk for at least 20 minutes a day!"
-    elif month == "february":
-        challenge_text = "Learn 200 words of a new language!"
-    elif month == "march":
-        challenge_text = "Follow a vegetarian diet for the whole month!"
-    else:
-        challenge_text = "This month is not supported!"
-        HttpResponseNotFound(challenge_text)
-    return HttpResponse(challenge_text)
+    try:
+        challenge_text = monthly_challenges[month]
+        return HttpResponse(challenge_text)
+    except:
+        return HttpResponseNotFound("This month is not supported!")
