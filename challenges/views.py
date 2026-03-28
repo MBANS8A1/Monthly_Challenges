698c7d4678cfdbd21ec1251a1af29392ec739fcf
MBANS8A1 <seanmbng@aol.com>
2026-03-28 01:27:12 +0000

    In order to avoid hardcoded redirect paths, I decided to make use of the 3rd argument provided by the path function in challenges/urls.py. With the name argument I gave my path to the monthly_challenge view function a name of "month-challenge". Next, within challenges/views.py, I used constructed an absolute path reference with the reverse function using the url path name and the args list with the redirect_month.; the redirect_path variable is then passed to the HttpResponseRedirect object. So now, in the main project urls.py I can change the portion of path ahead of the domain to any path string I like and it will capture the dynamic segments I added to the paths in challenges/urls.py.

--- challenges/views.py
+++ challenges/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
 # Create your views here.
 
 monthly_challenges = {
@@ -25,7 +26,8 @@ def monthly_challenge_by_number(request, month):
         return HttpResponseNotFound("Invalid month")
     # Subtract 1 becauses lists are zero-indexed
     redirect_month = named_months[month-1]
-    return HttpResponseRedirect(f"/challenges/{redirect_month}")
+    redirect_path = reverse("month-challenge", args=[redirect_month])
+    return HttpResponseRedirect(redirect_path)
 
 
 def monthly_challenge(request, month):
