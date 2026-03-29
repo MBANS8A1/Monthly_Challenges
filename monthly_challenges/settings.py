8619686ae3933e5f67ffa444235379361c24afaf
MBANS8A1 <seanmbng@aol.com>
2026-03-29 18:35:38 +0100

    I forgot a comma on line 34 in settings.py after I registered my challenges app with django.

--- monthly_challenges/settings.py
+++ monthly_challenges/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'challenges'
+    'challenges',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
