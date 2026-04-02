5ac09bad16e340ecab092a4a7a08db1ae6d6e454
MBANS8A1 <seanmbng@aol.com>
2026-04-02 20:03:00 +0100

    I stored the parsed 404.html template from render_to_string in the monthly_challenge view function within a response_data variable and added that variable to the HttpResponseNotFound object.

--- challenges/views.py
+++ challenges/views.py
@@ -46,5 +46,5 @@ def monthly_challenge(request, month):
             "month_name": month
         })
     except:
-        render_to_string("404.html")
-        return HttpResponseNotFound("<h1>This month is not supported!</h1>")
+        response_data = render_to_string("404.html")
+        return HttpResponseNotFound(response_data)
