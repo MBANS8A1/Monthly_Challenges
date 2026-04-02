4ce7344b71b160bde85bedd099152fb15be22de6
MBANS8A1 <seanmbng@aol.com>
2026-04-02 17:49:05 +0100

    The 404.html file has been rendered as a string in the except block of the monthly_challenge view function.

--- challenges/views.py
+++ challenges/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
+from django.template.loader import render_to_string
 # Create your views here.
 
 monthly_challenges = {
@@ -45,4 +46,5 @@ def monthly_challenge(request, month):
             "month_name": month
         })
     except:
+        render_to_string("404.html")
         return HttpResponseNotFound("<h1>This month is not supported!</h1>")
